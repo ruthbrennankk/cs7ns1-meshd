b008f6c9452929518025864820490c64c1f7412c
ruthbrennankk <brennar5@tcd.ie>
2021-11-25 13:47:58 +0000

    communication after discovery peer to peer updates

--- meshd/sensor.py
+++ meshd/sensor.py
@@ -1,95 +1,15 @@
 import socket
-import random
-import struct
-
-from utils import hash_payload
 
 class Sensor:
-
-    # generates intial params on start
-    # randomly progresses 'vehicle' down path during each data collection cycle
-    # generates other params randomly.
-
     def __init__(self, sensorType, session, port):
         self.sensorType = sensorType
         self.session = session
         self.port = port
 
-        self.x_pos = random.randint(20,80)
-        self.y_pos = random.randint(20,80)
-        self.x_lim = 100
-        self.y_lim = 100 # defined city block range for travel
-        self.journey_len = random.randint(20,50)
-        self.pressure = 35
-        self.current_step = 0
-
-        self.sync_node_host = ''
-        self.sync_node_port = 33215
-
-
     def generate_data(self, sensorType):
-        metricMap = {'position': self.getPos(),
-           'temperature': self.getTemp(),
-           'tyre_pressure': self.getPressure(),
-           'journey_elapsed': self.getElapsed(),
-           'journey_finished': self.getStatus()}
-
-
-        print('Collecting data from sensor type:', (sensorType))
-        if sensorType not in metricMap:
-            print('Invalid sensor type found')
-            return None
-        return metricMap.get(sensorType)
+        print('Collecting/Generating Data for sensor type:', (sensorType))
+        return None
 
     def send_data(self, data):
-        try:
-            print('Sending Data to Sync Node \n')
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(p)
-            hash = hash_payload(data)
-            packet = struct.pack('!32s%ds' % len(data), hash, data)
-            sock.send(packet)
-            sock.close()
-        except:
-            print('Sending data to sync node failed. \n')
-
-    def getPressure(self):
-        self.pressure = self.pressure - random.uniform(0, 0.5)
-        return self.pressure
-
-    def getTemp(self):
-        return random.uniform(5, 35)
-
-    def getPos(self):
-        if self.current_step < self.journey_len:
-            self.makeMove()
-            self.current_step += 1
-        return (self.x_pos, self.y_pos)
-
-    def getElapsed(self):
-        return self.current_step
-
-    def getStatus(self):
-        if self.current_step < self.journey_len:
-            return False
-        return True
-
-    def makeMove(self):
-        dir = random.randint(1,2) # x or y movement in given move
-        move_len =  random.randint(1,3)
-        disp = [-1,1][random.randrange(2)] * move_len
-        if dir == 1:
-            self.y_pos += disp
-        else:
-            self.x_pos += disp
-        self.limitPosition()
-
-    def limitPosition(self): # stay in range
-        if self.x_pos > self.x_lim:
-            self.x_pos = self.x_lim
-        if self.y_pos > self.y_lim:
-            self.y_pos = self.y_lim
-        if self.x_pos < 0:
-            self.x_pos = 0
-        if self.y_pos < 0:
-            self.y_pos = 0
+        print('Sending Data to Sync Node \n')
+        return None
