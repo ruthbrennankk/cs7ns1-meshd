401a559ff2493252430a8c40073da0207abd81e4
Mahislami <61377584+Mahislami@users.noreply.github.com>
2021-11-27 13:26:23 +0000

    Update runme.sh
    Adding sensors_deamon.py to the file

--- meshd/runme.sh
+++ meshd/runme.sh
@@ -1,8 +1,37 @@
-echo "Creating nodes running in background"
- for (( i=0; i<=3; i++ ))
- do
-     nohup python3 daemon.py </dev/null >/dev/null 2>&1 &
- done
+echo "---FOUR NODES IN BACKGROUND---"
+for (( i=1; i<=4; i++ ))
+do
+    nohup python3 daemon.py --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    echo "Node in Created at"
+    echo 3321$i
+    nohup python3 sensor_daemon.py --sensortype position --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype temperature --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype journey_elapsed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype journey_finished --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype fuel --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype speed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype wind --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    nohup python3 sensor_daemon.py --sensortype humidity --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+done
 
- echo "Creating a node in foreground"
- python3 daemon.py
+echo "---ONE NODE IN FOREGROUND---"
+echo "Node in Created at"
+echo 33215
+python3 daemon.py --sensorport 33215
+python3 sensor_daemon.py --sensortype position --sensorport 33215
+echo "Position sensor ready"
+python3 sensor_daemon.py --sensortype temperature --sensorport 33215
+echo "Temperature sensor ready"
+python3 sensor_daemon.py --sensortype journey_elapsed --sensorport 33215
+echo "Journey Timer sensor ready"
+python3 sensor_daemon.py --sensortype journey_finished --sensorport 33215
+echo "Journey Finished sensor ready"
+python3 sensor_daemon.py --sensortype fuel --sensorport 33215
+echo "Fuel sensor ready"
+python3 sensor_daemon.py --sensortype speed --sensorport 33215
+echo "Speed sensor ready"
+python3 sensor_daemon.py --sensortype wind --sensorport 33215
+echo "Wind sensor ready"
+python3 sensor_daemon.py --sensortype humidity --sensorport 33215
+echo "Humidity sensor ready"
+
