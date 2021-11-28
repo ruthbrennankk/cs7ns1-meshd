51f484ff706dc6b41d55a80267d795653f1b9958
ruthbrennankk <brennar5@tcd.ie>
2021-11-28 17:00:49 +0000

    tester

--- meshd/runme.sh
+++ meshd/runme.sh
@@ -1,36 +1,14 @@
-echo "---FOUR NODES IN BACKGROUND---"
-for (( i=1; i<=4; i++ ))
-do
-    nohup python3 daemon.py --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    echo "Node in Created at"
-    echo 3321$i
-    nohup python3 sensor/sensor_daemon.py --sensortype position --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype temperature --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype fuel --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype speed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype wind --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    nohup python3 sensor/sensor_daemon.py --sensortype humidity --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-done
+# PORT RANGE
 
-echo "---ONE NODE IN FOREGROUND---"
-echo "Node in Created at"
-echo 33215
+
+# START SYNC/PEER NODE
 python3 daemon.py --sensorport 33215
-nohup python3 sensor/sensor_daemon.py --sensortype position --sensorport 33215
-echo "Position sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype temperature --sensorport 33215
-echo "Temperature sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport 33215
-echo "Journey Timer sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport 33215
-echo "Journey Finished sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype fuel --sensorport 33215
-echo "Fuel sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype speed --sensorport 33215
-echo "Speed sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype wind --sensorport 33215
-echo "Wind sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype humidity --sensorport 33215
-echo "Humidity sensor ready"
+# START SYNC/PEER NODE - with sensor status
+python3 daemon.py --sensorport 33215 --printsensors True
+
+# START GENERATION/SENSOR NODE
+python3 sensor/sensor_daemon.py --sensortype position --sensorport 33215
+# TYPE OPTIONS
+# [temperature, journey_elapsed, journey_finished, fuel, speed, humidity]
+
+
--- meshd/runme_background.sh
+++ meshd/runme_background.sh
@@ -1,14 +1,14 @@
 echo "Starting 2 background nodes and their sensors"
-for (( i=1; i<=2; i++ ))
+for (( i=1; i<=2; i++))
 do
-    python3 daemon.py --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    python3 daemon.py --sensorport $1+$i </dev/null >/dev/null 2>&1 &
     echo "Node in Created at"
-    echo 3321$i
-    python3 sensor/sensor_daemon.py --sensortype position --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype temperature --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype fuel --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype speed --sensorport 3321$i </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype humidity --sensorport 3321$i </dev/null >/dev/null 2>&1 &
+    echo $1+$i
+    python3 sensor/sensor_daemon.py --sensortype position --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype temperature --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype fuel --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype speed --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    python3 sensor/sensor_daemon.py --sensortype humidity --sensorport $1+$i </dev/null >/dev/null 2>&1 &
 done
