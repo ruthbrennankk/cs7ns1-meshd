4a4abdca814a2429dd6b89b4a06290ff9c6267e2
ruthbrennankk <brennar5@tcd.ie>
2021-11-28 15:57:15 +0000

    tester

--- meshd/runme_node1.sh
+++ meshd/runme_node1.sh
@@ -1,16 +1,3 @@
-echo "Node in Created at 33201"
-python3 daemon.py --sensorport 33201
-nohup python3 sensor/sensor_daemon.py --sensortype position --sensorport 33201
-echo "Position sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport 33201
-echo "Journey Timer sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport 33201
-echo "Journey Finished sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype fuel --sensorport 33201
-echo "Fuel sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype speed --sensorport 33201
-echo "Speed sensor ready"
-nohup python3 sensor/sensor_daemon.py --sensortype humidity --sensorport 33201
-echo "Humidity sensor ready"
-echo "Set up Temperature Sensor"
-#nohup python3 sensor/sensor_daemon.py --sensortype temperature --sensorport 33201
+echo "Node in Created at $1"
+python3 daemon.py --sensorport $1
+echo "Start Sensors"
---
+++ meshd/runme_sensors.sh
@@ -0,0 +1,14 @@
+nohup python3 sensor/sensor_daemon.py --sensortype position --sensorport $1
+echo "Position sensor ready"
+nohup python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport $1
+echo "Journey Timer sensor ready"
+nohup python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport $1
+echo "Journey Finished sensor ready"
+nohup python3 sensor/sensor_daemon.py --sensortype fuel --sensorport $1
+echo "Fuel sensor ready"
+nohup python3 sensor/sensor_daemon.py --sensortype speed --sensorport $1
+echo "Speed sensor ready"
+nohup python3 sensor/sensor_daemon.py --sensortype humidity --sensorport $1
+echo "Humidity sensor ready"
+echo "Set up Temperature Sensor"
+#nohup python3 sensor/sensor_daemon.py --sensortype temperature --sensorport 33201
