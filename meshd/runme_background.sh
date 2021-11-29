f863dae6770e72bddc7e99b9ff9d4d346d384b1c
ruthbrennankk <brennar5@tcd.ie>
2021-11-29 10:00:53 +0000

    tester

--- meshd/runme_background.sh
+++ meshd/runme_background.sh
@@ -1,9 +1,9 @@
 echo "Starting 2 background nodes and their sensors"
 for (( i=1; i<=2; i++))
 do
+    echo "Node Sensor Port"
+    echo j=$((i + $1))
     python3 daemon.py --sensorport $1+$i </dev/null >/dev/null 2>&1 &
-    echo "Node in Created at"
-    echo $1+$i
     python3 sensor/sensor_daemon.py --sensortype position --sensorport $1+$i </dev/null >/dev/null 2>&1 &
     python3 sensor/sensor_daemon.py --sensortype temperature --sensorport $1+$i </dev/null >/dev/null 2>&1 &
     python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport $1+$i </dev/null >/dev/null 2>&1 &
@@ -11,4 +11,5 @@ do
     python3 sensor/sensor_daemon.py --sensortype fuel --sensorport $1+$i </dev/null >/dev/null 2>&1 &
     python3 sensor/sensor_daemon.py --sensortype speed --sensorport $1+$i </dev/null >/dev/null 2>&1 &
     python3 sensor/sensor_daemon.py --sensortype humidity --sensorport $1+$i </dev/null >/dev/null 2>&1 &
+    echo "Node Created at $j"
 done
