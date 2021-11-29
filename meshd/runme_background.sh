a5a4cb7ba0ee3028bd381a25984a25346e440e23
ruthbrennankk <brennar5@tcd.ie>
2021-11-29 10:22:21 +0000

    tester

--- meshd/runme_background.sh
+++ meshd/runme_background.sh
@@ -1,10 +1,11 @@
 echo "Creating Node with Sensor Port $1"
-    python3 daemon.py --sensorport $1 >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype position --sensorport $1 >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype temperature --sensorport $1 >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport $1 >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport $1 </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype fuel --sensorport $1 </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype speed --sensorport $1 </dev/null >/dev/null 2>&1 &
-    python3 sensor/sensor_daemon.py --sensortype humidity --sensorport $1 </dev/null >/dev/null 2>&1 &
+python3 daemon.py --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype position --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype temperature --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype journey_elapsed --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype journey_finished --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype fuel --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype package_id --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype speed --sensorport $1 >/dev/null 2>&1 &
+python3 sensor/sensor_daemon.py --sensortype humidity --sensorport $1 >/dev/null 2>&1 & #</dev/null 
 echo "Node Created and Sensors Started"
